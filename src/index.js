2447773b9d6f60ba5a8b036157c16e869cc43244
Daniel Kenney <danielkenney99@yahoo.com>
2022-11-23 14:18:06 -0800

    added styling and filter option

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import Cardlist from "./CardList";
-import { robots } from "./robots";
+import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Cardlist robots={robots} />
+    <App />
   </React.StrictMode>
 );
 
